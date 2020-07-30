7bdc5c9dfc6e4bdd8be4caa148cb041c27ecf62e
Asheley Brown <ABrown119@humana.com>
2020-07-29 20:47:25 -0400

    Backend Updated

--- models/products.js
+++ models/products.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 
 const productsSchema = mongoose.Schema({
-    name: String,
+    products_name: String,
     price: String,
     descriptions: String
 
+});
+
+const thesisSchema = mongoose.Schema({
+	products: String, 
+	user: [{//Referenced
+		type: mongoose.Schema.ObjectId, 
+		ref: 'user'
+	}], 
+	orderDetails: [orderDetailsSchema]//Embedded
 });
 
 const products = mongoose.model('products', productsSchema);
